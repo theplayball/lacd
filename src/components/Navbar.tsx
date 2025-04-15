7eda82bd781eadbe9b1f74a5c8ce14a95fad390e
MarcelinaMakhoul <marcelina.makhoul@theplayball.com>
2025-04-15 18:31:06 +0300

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,58 @@
+"use client";
+import Link from "next/link";
+import SideMenu from "./SideMenu";
+import { usePathname } from "next/navigation";
+
+export default function Navbar() {
+  const pathname = usePathname();
+
+  return (
+    <header className="fixed inset-x-0 top-0 z-50 w-full  ">
+      
+      <div className="bg-[#1D3557] text-white flex flex-col  md:flex-row items-center justify-between px-8 py-6 gap-4">
+        
+        <div className="flex flex-col items-center justify-between">
+          <h1 className="text-4xl font-bold leading-tight">
+            <span className="italic font-cursive">LCAD</span>
+          </h1>
+          <p className="text-sm italic text-gray-300 mt-1 items-center justify-between">
+            “Nam elit agna, enderit sit amet, tincidunt ac, viverra sed, nulla..”
+          </p>
+        </div>
+        <div className="bg-gray-400/10 p-4 rounded-md flex flex-col items-center justify-between gap-1 w-full max-w-md">
+          <span className="uppercase text-sm font-medium"><span className="font-bold">Sign Up</span> for Email Updates</span>
+          <div className="flex flex-col sm:flex-row gap-2 mt-1">
+            <input
+              type="email"
+              placeholder="Email address"
+              className="px-3 py-2 rounded-sm bg-white text-gray-400 text-sm w-full"
+            />
+            <input
+              type="text"
+              placeholder="Zip code"
+              className="px-3 py-2 rounded-sm bg-white text-gray-400 text-sm w-full sm:w-24"
+            />
+            <button className="bg-[#4EA8DE] text-white px-4 py-2 rounded-sm text-3xl w-full sm:w-auto">
+              →
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-gray-100 text-black w-full px-6 flex items-center border-1 border-gray-300">
+       
+        <div className="md:hidden flex items-center gap-2">
+          <SideMenu />
+        </div>
+
+        <div className="hidden md:flex gap-6 text-base font-semibold ml-auto">
+          <Link href="/" className="hover:underline">HOME</Link>
+          <Link href="/" className="hover:underline">ABOUT LCAD</Link>
+          <Link href="/" className="hover:underline">ACTIVITIES</Link>
+          <Link href="/" className="hover:underline">CONTACT US</Link>
+          <Link href="/" className="hover:underline">DONATION</Link>
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ src/components/SideMenu.tsx
@@ -0,0 +1,86 @@
+"use client";
+import React, { useState, useRef, useEffect } from "react";
+
+const SideMenu = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+        setMenuOpen(false);
+      }
+    };
+
+    if (menuOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuOpen]);
+
+  const navLinks = [
+    { label: "Home", href: "#home" },
+    { label: "About LCAD", href: "#about" },
+    { label: "Activities", href: "#activities" },
+    { label: "Contact Us", href: "#contact" },
+    { label: "Donation", href: "#donation" },
+  ];
+
+  return (
+    <div className="md:hidden relative w-full z-50" ref={menuRef}>
+ 
+  <button
+    onClick={toggleMenu}
+    className=" text-gray-600 w-full px-2 py-3 flex items-center gap-2"
+    aria-expanded={menuOpen}
+  >
+    <svg
+      className="w-6 h-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M4 6h16M4 12h16M4 18h16"
+      />
+    </svg>
+    <span className="font-medium text-lg">Menu</span>
+  </button>
+
+  {menuOpen && (
+    <div className="absolute left-0 w-screen pr-10 bg-gray-100 border-t border-gray-300 shadow-lg">
+    {navLinks.map((item, index) => (
+      <a
+        key={item.label}
+        href={item.href}
+        onClick={() => setMenuOpen(false)}
+        className={"flex items-center border-[0.5px] border-gray-200 justify-between px-4 py-3 hover:bg-sky-200 "}
+      >
+        <span>{item.label}</span>
+      </a>
+    ))}
+  
+    <a
+      href="#donation"
+      onClick={() => setMenuOpen(false)}
+      className="block text-white bg-red-700 px-4 py-3 text-center font-semibold hover:bg-red-800"
+    >
+      DONATE TODAY
+    </a>
+  </div>
+  
+  )}
+</div>
+
+  );
+};
+
+export default SideMenu;
